1be80fc6f43dc58180e0c1810ee2684761c12447
lenrek88 <lenrek88@yandex.ru>
2022-01-03 22:44:34 +0300

    add Forecast

--- Script/script.js
+++ Script/script.js
@@ -4,7 +4,6 @@ let now = document.querySelector('#tab-btn-1')
 let details = document.querySelector('#tab-btn-2')
 let forecast = document.querySelector('#tab-btn-3')
 
-
 function displayOn() {
     let contentDivOne = document.querySelector('.content_1')
     let contentDivTwo = document.querySelector('.content_2')
@@ -28,6 +27,7 @@ function displayOn() {
             contentDivThree.id = 'active';
             contentDivTwo.id = '';
             contentDivOne.id = '';
+            forecastF();
             break;
     }
 }
@@ -57,14 +57,17 @@ function isSearchButtonHandler(event) {
     const thisCityDetails = document.querySelector('.detailsCity');
 
     const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+
     const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-    const cityName = searchBtn.previousElementSibling.value
+    const cityName = searchBtn.previousElementSibling.value;
     thisCityDetails.textContent = cityName;
     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
+
 
     localStorage.setItem('ThisCity', searchBtn.previousElementSibling.value);
 
     event.preventDefault();
+
 
     fetch(url)
         .then(response => response.json())
@@ -112,6 +115,9 @@ function isSearchButtonHandler(event) {
                 alert(inf.message)
             }
     })
+
+
+
     .catch(err => alert(err));
 
     const locationDiv = document.querySelectorAll('.cityLike');
@@ -126,6 +132,111 @@ function isSearchButtonHandler(event) {
     }
 
 }
+
+
+// Forecast
+
+function forecastF() {
+    const deleteDiv = document.querySelectorAll('.forecast');
+    for (elem of deleteDiv) {
+        elem.remove();
+    }
+    const serverUrl2 = 'http://api.openweathermap.org/data/2.5/forecast';
+    const cityName = searchBtn.previousElementSibling.value;
+    const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+    const thisCityDetails = document.querySelector('.detailsCity2');
+    thisCityDetails.textContent = cityName;
+    let content3 = document.querySelector('.content_3')
+
+
+
+    const url2 = `${serverUrl2}?q=${cityName}&appid=${apiKey}`;
+
+    fetch(url2)
+        .then(response => response.json())
+        .then(inf => {
+            inf.list.forEach(function(item,i,arr) {
+                const pDate = document.createElement('p');
+                pDate.classList.add('Date');
+                pDate.textContent = '1';
+                let date = new Date(item.dt*1000);
+                let date2 = date.getDate();
+                const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
+                    "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+                let date3 = monthNames[date.getMonth()];
+                pDate.textContent = `${date2} ${date3}`;
+
+                const pTime = document.createElement('p');
+                pTime.classList.add('Time');
+                let hours = date.getHours();
+                if (hours < 10) {
+                    hours = "0" + hours
+                }
+                let minutes = date.getMinutes();
+                if (minutes < 10) {
+                    minutes = "0" + minutes
+                }
+                pTime.textContent = `${hours}:${minutes}`
+
+
+                const pTemp = document.createElement('p');
+                pTemp.classList.add('temp')
+                let temp = item.main.temp-273.15;
+                pTemp.innerHTML = `Temperature: ${Math.floor(temp)} &deg`;
+
+
+                const pRain = document.createElement('p');
+                pRain.classList.add('rain');
+                pRain.textContent = item.weather[0].main;
+
+
+                const pfeelsTemp = document.createElement('p');
+                pfeelsTemp.classList.add('feelsTemp');
+                let feelsLike = item.main.feels_like-273.15;
+                pfeelsTemp.innerHTML = `Feels like: ${Math.floor(feelsLike)} &deg`;
+
+
+                const thisCloud = document.createElement('div');
+                thisCloud.classList.add('thisCloud');
+
+                if (item.weather[0].id === 800) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/01d@2x.png)';
+                } else if (item.weather[0].id === 801) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/02d@2x.png)';
+                } else if (item.weather[0].id === 802) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/03d@2x.png)';
+                } else if (item.weather[0].id === 803 || item.weather[0].id === 804) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/04d@2x.png)';
+                } else if (item.weather[0].id > 199 && item.weather[0].id < 233) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/11d@2x.png)';
+                } else if (item.weather[0].id > 299 && item.weather[0].id < 322) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/09d@2x.png)';
+                } else if (item.weather[0].id > 499 && item.weather[0].id < 505) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/10d@2x.png)';
+                } else if (item.weather[0].id === 511) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/13d@2x.png)';
+                } else if (item.weather[0].id > 519 && item.weather[0].id < 532) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/09d@2x.png)';
+                } else if (item.weather[0].id > 599 && item.weather[0].id < 623) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/09d@2x.png)';
+                } else if (item.weather[0].id > 700 && item.weather[0].id < 782) {
+                    thisCloud.style.backgroundImage = 'url(http://openweathermap.org/img/wn/50d@2x.png)';
+                }
+
+
+                const div = document.createElement('div');
+                div.classList.add('forecast');
+                content3.append(div)
+                div.append(pDate);
+                div.append(pTime);
+                div.append(pTemp);
+                div.append(pRain);
+                div.append(pfeelsTemp);
+                div.append(thisCloud);
+            })
+        })
+}
+
 
 
 // Добавление в избранное
@@ -229,9 +340,10 @@ function isLikeToNow() {
 
     searchBtn.previousElementSibling.value = this.textContent;
     isSearchButtonHandler(event);
+    forecastF();
+
 
     likeButton.style.backgroundImage = 'url(../Style/ShapeLike.svg)'
-
 
 }
 
