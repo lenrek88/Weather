0a089b50fcc8e2760a2142ef67492a50b6e51252
lenrek88 <lenrek88@yandex.ru>
2022-01-03 16:00:04 +0300

    add likeCity and RemoveLikeCity

--- Script/script.js
+++ Script/script.js
@@ -41,6 +41,8 @@ forecast.addEventListener('click', displayOn)
 
 // Запрос погоды
 
+const likeButton = document.querySelector('.like');
+likeButton.addEventListener('click', isLikeButtonHandler);
 
 
 
@@ -59,6 +61,7 @@ function isSearchButtonHandler(event) {
     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
 
     event.preventDefault()
+
     fetch(url)
         .then(response => response.json())
         .then(inf => {
@@ -71,18 +74,30 @@ function isSearchButtonHandler(event) {
             }
     })
     .catch(err => alert(err));
+
+    const locationDiv = document.querySelectorAll('.cityLike');
+    likeButton.style.backgroundImage = 'url(../Style/Shape.svg)';
+
+    for (elem of locationDiv) {
+        if (cityName === elem.firstElementChild.textContent) {
+            likeButton.style.backgroundImage = 'url(../Style/ShapeLike.svg)';
+            break;
+            return;
+        }
+    }
+
 }
 
 
 // Добавление в избранное
 
-const counter = 2;
+let counter = 1;
 
-const likeButton = document.querySelector('.like');
-likeButton.addEventListener('click', isLikeButtonHandler);
+const deleteButton = document.querySelector('.delete');
+const cityLikeName = document.querySelector('.cityLikeName')
+deleteButton.addEventListener('click', isButtonDeleteHandler);
+cityLikeName.addEventListener('click', isLikeToNow)
 
-
-// TODO Написать h2 button стили id к locations div
 
 function isLikeButtonHandler(event) {
 
@@ -91,17 +106,64 @@ function isLikeButtonHandler(event) {
     const location = document.querySelector('.locations');
     const nameThisCity = document.querySelector('.thisCity');
     const isNameThisCity = nameThisCity.textContent;
+    const locationDiv = document.querySelectorAll('.cityLike')
 
+    for (elem of locationDiv) {
+        if (elem.firstElementChild.textContent === isNameThisCity) {
+            console.log(elem.firstElementChild.textContent)
+            alert('Данный город уже присутствует в избранном!');
+            return;
+            break;
+        }
+    }
 
+    likeButton.style.backgroundImage = 'url(../Style/ShapeLike.svg)'
+
+    console.log(elem.firstElementChild.textContent)
     const div = document.createElement('div');
     div.classList.add('cityLike');
-    div.textContent = isNameThisCity;
+    div.id = counter;            counter = ++counter;
+
+    const h2 = document.createElement('h2');
+    h2.textContent = isNameThisCity;
+    h2.addEventListener('click', isLikeToNow);
+    div.append(h2);
+
+    const button = document.createElement('button');
+    button.classList.add('delete');
+    button.addEventListener('click', isButtonDeleteHandler)
+    div.append(button);
+
     location.append(div);
 
+}
 
-    counter = ++counter;
+// Удаление из избранного
+
+function isButtonDeleteHandler() {
+    const nameThisCity = document.querySelector('.thisCity');
+    if (nameThisCity.textContent === this.previousElementSibling.textContent) {
+        likeButton.style.backgroundImage = 'url(../Style/Shape.svg)'
+    }
+
+    this.parentElement.remove()
+
 
 }
+
+// Чтение из избранного
+
+function isLikeToNow() {
+
+    searchBtn.previousElementSibling.value = this.textContent;
+    isSearchButtonHandler(event);
+
+    likeButton.style.backgroundImage = 'url(../Style/ShapeLike.svg)'
+
+
+}
+
+
 
 
 
